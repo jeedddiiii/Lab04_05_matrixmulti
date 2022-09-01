0c0f33a44de542330f9dad8083b46bb3e6f0f3f8
jeedddiiii <pendigon.once@gmail.com>
2022-09-02 06:59:10 +0700

    MatrixMultiplication

---
+++ src/matrix/multi/Matrix.java
@@ -0,0 +1,34 @@
+package matrix.multi;
+
+
+public class Matrix {
+    private final int row;
+    private final int col;
+    private final int[][] matrix;
+
+    public Matrix(int row, int col) {
+        this.row = row;
+        this.col = col;
+        matrix = new int[row][col];
+    }
+
+    public Matrix(int[][] matrix) {
+        this.matrix = matrix;
+        this.row = matrix.length;
+        this.col = matrix[0].length;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+}
+
+
